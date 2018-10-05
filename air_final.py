03509c53179bdd14e351ccde38e5e396cf1e533d
Ryan Bond <drbond@gmail.com>
2018-10-05 10:29:32 -0500

    Removing pinned versions to test with python 3.x

--- air_final.py
+++ air_final.py
@@ -20,10 +20,11 @@ from sklearn.linear_model import LinearRegression
 def create_dataset(dataset, look_back=1):
     dataX, dataY = [], []
     for i in range(len(dataset)-look_back-1):
-		a = dataset[i:(i+look_back), 0]
-		dataX.append(a)
-		dataY.append(dataset[i + look_back, 0])
+        a = dataset[i:(i+look_back), 0]
+    dataX.append(a)
+    dataY.append(dataset[i + look_back, 0])
     return numpy.array(dataX), numpy.array(dataY)
+
 
 # fix random seed for reproducibility
 numpy.random.seed(7)
